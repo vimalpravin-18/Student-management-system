cdb891cd7307d6a684c4d085fe4ec4645122d082
Vimal pravin <vimalpravin071@gmail.com>
2026-04-14 22:09:51 +0530

    Update StudentController.java

--- Student Management system/backend/demo/src/main/java/com/example/studentapp/controller/StudentController.java
+++ Student Management system/backend/demo/src/main/java/com/example/studentapp/controller/StudentController.java
@@ -1,5 +1,40 @@
-package backend.demo.src.main.java.com.example.demo.controller;
+package com.example.studentapp.controller;
 
-public class StudentController {
-    
+import com.example.studentapp.model.Student;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@CrossOrigin(origins = "*")
+public class StudentController {    
+
+    private List<Student> students = new ArrayList<>();
+    private int idCounter = 1;
+
+    // ✅ GET all students
+    @GetMapping
+    public List<Student> getStudents() {
+        return students;
+    }   
+
+    // ✅ ADD student
+    @PostMapping   
+public Student addStudent(@RequestBody Student s) {
+    System.out.println("Received: " + s.getName() + " " + s.getMark());
+    s.setId(idCounter++);
+    students.add(s);
+    return s;
+}
+
+    // ✅ DELETE student
+    @DeleteMapping("/{id}")
+public String deleteStudent(@PathVariable int id) {
+
+    students.removeIf(s -> s.getId() == id);
+
+    return "Deleted";
+}
 }
